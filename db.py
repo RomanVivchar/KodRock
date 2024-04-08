31ec648c5942aea1c1433706dbc845e9b24f87ef
roman-vivchar <scrable5297@gmail.com>
2024-04-08 23:17:21 +0300

    added new class Database

--- db.py
+++ db.py
@@ -6,28 +6,33 @@ class Database:
         self.connection = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
         self.cursor = self.connection.cursor()
 
-    def add_user(self, telegram_id: str):
+    def add_user(self, telegram_id: int):
         with self.connection:
-            return self.cursor.execute("INSERT INTO user (telegram_id) VALUES (?)", (telegram_id,))
+            return self.cursor.execute("INSERT INTO public.user (user_id) VALUES (%s)", (telegram_id,))
 
-    def user_exists(self, telegram_id: str):
+    def user_exists(self, telegram_id: int):
         with self.connection:
-            result = self.cursor.execute("SELECT * FROM user WHERE telegram_id = ?", (telegram_id,)).fetchall()
-            return bool(len(result))
+            self.cursor.execute("SELECT * FROM public.user WHERE user_id = (%s)", (telegram_id,))
+            row = self.cursor.fetchall()
+            return bool(len(row))
 
-    def set_name(self, telegram_id: str, name: str):
+    def set_name(self, telegram_id: int, name: str):
         with self.connection:
-            return self.cursor.execute("UPDATE user SET name = ? WHERE telegram_id = ?", (name, telegram_id,))
+            return self.cursor.execute("UPDATE public.user SET name = (%s) WHERE user_id = (%s)", (name, telegram_id,))
 
-    def set_last_name(self, telegram_id: str, last_name: str):
+    def set_last_name(self, telegram_id: int, last_name: str):
         with self.connection:
-            return self.cursor.execute("UPDATE user SET last_name = ? WHERE telegram_id = ?", (last_name, telegram_id,))
+            return self.cursor.execute("UPDATE public.user SET last_name = (%s) WHERE user_id = (%s)", (last_name, telegram_id,))
 
-    def set_email(self, telegram_id: str, email: str):
+    def set_email(self, telegram_id: int, email):
         with self.connection:
-            return self.cursor.execute("UPDATE user SET email = ? WHERE telegram_id = ?"), (email, telegram_id,)
+            query = "UPDATE public.user SET email = (%s) WHERE user_id = (%s)"
+            data = (email, telegram_id,)
+            return self.cursor.execute(query, data)
 
-    def set_phone(self, telegram_id: str, phone: str):
+    def set_phone(self, telegram_id: int, phone):
         with self.connection:
-            return self.cursor.execute("UPDATE user SET phone_number = ? WHERE telegram_id = ?"), (phone, telegram_id,)
+            query = "UPDATE public.user SET phone_number = (%s) WHERE user_id = (%s)"
+            data = (phone, telegram_id,)
+            return self.cursor.execute(query, data)
 
